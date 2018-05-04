0e75986941bc142101210e638005f8afefe5f050
David Tran <davidtran19@yahoo.com>
2018-05-04 13:49:33 +0200

    Added comments on what to do

--- main.R
+++ main.R
@@ -1,8 +1,12 @@
 rm(list=ls())
 library(data.table)
 data_fri <- fread('comm-data-Fri.csv', header = T, sep = ',')
-counter = 0
+data_sat <- fread('comm-data-Sat.csv', header = T, sep = ',')
+data_sun <- fread('comm-data-Sun.csv', header = T, sep = ',')
 
-for( i in 1:length(data$from) ){
-  counter++
-}
+# Plot communication based on location and timestamp as x-axis.
+# Cluster communication based on location.
+# Find out person that communicates with multiple persons under a specific time frame.
+# Find out group based on if the communication has been frequent between people.
+# Longest common subsequence to determine who communicates with the most people under a sequence.
+# Flag suspect person based on communication patterns.
